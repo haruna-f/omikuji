0645e0f6a390a74b03cf8201b3836e85ac29c182
haruna-f <harunafujii.me@gmail.com>
2022-12-21 16:00:13 +0900

    upload:221221

--- src/js/modules/kotowaza.js
+++ src/js/modules/kotowaza.js
@@ -11,7 +11,8 @@ class Kotowaza {
 kotowazaArray[0] = new Kotowaza("笑う門には福来たる", "いつも笑いが溢れる人のまわりには、<br>自然と福が訪れます。<br/>あなたのステキな笑顔で<br>まわりの人も幸せになるでしょう。","0");
 kotowazaArray[1] = new Kotowaza("犬も歩けば棒に当たる", "今日は、思いがけず<br>いいことが起こるかも。<br>いろいろなことに<br>積極的にチャレンジしてみよう。", "01");
 kotowazaArray[2] = new Kotowaza("海老で鯛を釣る", "小さな投資で大きななにかを<br>得られることも。<br>宝くじを買ったら、<br>大当たりしちゃうかも⁈", "02");
-
+kotowazaArray[3] = new Kotowaza("早起きは三文の徳", "早く起きれば健康によく、<br>他にも良いことがあるでしょう。<br>明日こそ早起きして、ラジオ体操！<br>う〜ん…あと五分寝かせて…", images[3]);
+kotowazaArray[4] = new Kotowaza("河童の川流れ", "どんな得意なことも<br>失敗することもあるでしょう。<br>誰かの間違いに気がついたら、<br>さりげなくサポートするといいことがあるかも⁈", images[4]);
 
 // kotowazaArray[] = new Kotowaza("", "", images[]);
 // 案ずるよりも産むが易し
